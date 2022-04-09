ba44ff14dc28c9c535938ed0e07eff05cbe637f2
Kelvin Freitas <kelvin.arg11@gmail.com>
2022-04-09 01:56:39 -0300

    salvando dados e inicio tela de projetos

--- my-app/src/components/pages/NewProject.js
+++ my-app/src/components/pages/NewProject.js
@@ -6,32 +6,30 @@ import styles from './NewProject.module.css'
 function NewProject(){
     const history = useNavigate()
 
-    function createPost(project){
+    function createPost(project) {
         project.cost = 0
         project.services = []
-
-        fetch('http://localhost:5000/projects',
-        {method: "POST",
-        headers:{
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify(project)
+    
+        fetch('http://localhost:5000/projects', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(project),
         })
-        .then((resp) => resp.json())
-        .then((data) => {
-            console.log(data)
-            history.push('/projects',{  message: "Projeto criado com sucesso!"})
-        })
-        .catch(err => console.log(err))
-    }
-
-    return (
+          .then((resp) => resp.json())
+          .then((data) => {
+            history('/projects', { message: 'Projeto criado com sucesso!' })
+          })
+      }
+    
+      return (
         <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projeto para depois adicionar os serviços.</p>
-            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
+          <h1>Criar Projeto</h1>
+          <p>Crie seu projeto para depois adicionar os serviços</p>
+          <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
         </div>
-    )
+      )
 }
 
 export default NewProject
--- my-app/src/components/pages/Projects.js
+++ my-app/src/components/pages/Projects.js
@@ -1,6 +1,33 @@
+import { useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+import Message from "../layouts/Message"
+import Container from '../layouts/Container'
+import LinkButton from '../layouts/LinkButton'
+import ProjectCard from '../project/ProjectCard'
+
+import styles from './Projects.module.css'
+
 function Projects() {
+    const [projects,setProjects] = useState([])
+
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
+
     return (
-        <h1>Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Novo Projeto" />
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClaass="start">
+                <p>Projetos..</p>
+            </Container>
+        </div>
     )
 }
 
